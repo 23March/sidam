f38d6bf269c0f62c0ae0dd6008e9c06776730540
23March <kaitokid456@gmail.com>
2021-06-17 15:11:35 +0700

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,5 +1,5 @@
 <?php 
-$koneksi = mysqli_connect("localhost","root","","akademik");
+$koneksi = mysqli_connect("us-cdbr-east-04.cleardb.com","b80ed02f2de13b","48f3d0c7","heroku_1b9272f6840042f");
  
 // Check connection
 if (mysqli_connect_errno()){
